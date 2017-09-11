a0c2727e66059189b20529424672e429af701826
dell-master-linux <mayuanucas@qq.com>
2017-09-11 20:17:03 +0800

    贪心算法-完美字符串

--- src/Main.java
+++ src/Main.java
@@ -1,17 +1,14 @@
 import java.util.Scanner;
 
 public class Main {
-    public static void main(String[] args){
-//        Scanner sc = new Scanner(System.in);
-//
-//        while (sc.hasNext()){
-//
-//            System.out.println("hello java");
-//        }
-        String ce = "libai";
-        String[] temp = ce.split("/");
-        System.out.println(temp.length);
-        System.out.println(temp[0]);
 
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+
+        while (sc.hasNext()){
+
+
+
+        }
     }
 }
