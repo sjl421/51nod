eeb1e4902720598ae241ed640b885f44918ba99a
iie-pc <mayuanucas@qq.com>
2017-09-11 13:50:37 +0800

    源代码目录

---
+++ src/Main.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args){
+//        Scanner sc = new Scanner(System.in);
+//
+//        while (sc.hasNext()){
+//
+//            System.out.println("hello java");
+//        }
+        String ce = "libai";
+        String[] temp = ce.split("/");
+        System.out.println(temp.length);
+        System.out.println(temp[0]);
+
+    }
+}
