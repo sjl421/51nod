5971557d80781a8f29c7ca25cacd9c2978052b48
dell-master-linux <mayuanucas@qq.com>
2017-09-17 19:20:37 +0800

    代码输入结构

--- ac/Solution.java
+++ ac/Solution.java
@@ -4,12 +4,7 @@ public class Solution {
 
     public static void main(String[] args){
         Scanner sc = new Scanner(System.in);
-        while (sc.hasNext()){
 
-
-
-
-
-        }
+        
     }
 }
