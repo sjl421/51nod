172e837f8776522cc533e6172c158057144a89b6
dell-master-linux <mayuanucas@qq.com>
2017-09-22 00:27:32 +0800

    每天刷题

--- ac/动态规划基础/最大字段和/Solution.java
+++ ac/动态规划基础/最大字段和/Solution.java
@@ -14,7 +14,7 @@ public class Solution {
         int answerStart = 0, answerEnd = 0;     //最大子数组的开始位置 结束位置
         long answerMax = (long)numbers[0];     //最大子数组的总和
         int currentStart = 0;                   //当前子数组开始位置 (当前子数组结束位置可以由i得到)
-        long leftArraySum = (long)numbers[0];    //左侧相邻子数组的和(注意！ 左侧相邻子数组的和可以会大于int的表示范围)
+        long leftArraySum = (long)numbers[0];    //左侧相邻子数组的和(注意！ 左侧相邻子数组的和可能会大于int的表示范围)
         for(int i=1; i<n; i++){
             if(0 < leftArraySum){
                 leftArraySum += numbers[i];
