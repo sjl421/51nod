41499579c6b7e959e6fc5821bf0ba43e8903e2ae
dell-master-linux <mayuanucas@qq.com>
2017-10-17 20:50:59 +0800

    51nod刷题

---
+++ src/Solution.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+/**
+ * @author alpha
+ * @date 2017/10/17
+ */
+public class Solution {
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+
+
+
+    }
+}
