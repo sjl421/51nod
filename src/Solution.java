ad5523339b79fea5ec751287f24e88eb69c816a8
dell-master-linux <mayuanucas@qq.com>
2017-10-26 20:42:03 +0800

    代码模板

---
+++ src/Solution.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+/**
+ * @author alpha
+ * @date 2017/10/26
+ */
+public class Solution {
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+
+    }
+}
