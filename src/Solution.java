57f109f301961b3bf6c0770553e5a5bf76cae234
dell-master-linux <mayuanucas@qq.com>
2017-10-27 19:06:33 +0800

    字段求和

--- src/Solution.java
+++ src/Solution.java
@@ -8,5 +8,23 @@ public class Solution {
     public static void main(String[] args){
         Scanner sc = new Scanner(System.in);
 
+        int n = sc.nextInt();
+        int[] arrayNumber = new int[n];
+
+        for (int i=0; i<n; i++){
+            arrayNumber[i] = sc.nextInt();
+        }
+
+        int q = sc.nextInt();
+        while (0 < q--){
+            int begin = sc.nextInt();
+            int len = sc.nextInt();
+            long sum = 0;
+            for (int i=begin-1; i<begin+len-1; i++){
+                sum += arrayNumber[i];
+            }
+            System.out.println(sum);
+        }
+
     }
 }
