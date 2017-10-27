a6ef364142201a781a7714e87407b2777e95223c
dell-master-linux <mayuanucas@qq.com>
2017-10-27 19:15:13 +0800

    子段求和

--- src/Solution.java
+++ src/Solution.java
@@ -9,21 +9,18 @@ public class Solution {
         Scanner sc = new Scanner(System.in);
 
         int n = sc.nextInt();
-        int[] arrayNumber = new int[n];
+        // 数组中保存的是前n项的和
+        long[] arrayNumber = new long[n+1];
 
-        for (int i=0; i<n; i++){
-            arrayNumber[i] = sc.nextInt();
+        for (int i=1; i<=n; i++){
+            arrayNumber[i] = arrayNumber[i-1] + sc.nextLong();
         }
 
         int q = sc.nextInt();
         while (0 < q--){
             int begin = sc.nextInt();
             int len = sc.nextInt();
-            long sum = 0;
-            for (int i=begin-1; i<begin+len-1; i++){
-                sum += arrayNumber[i];
-            }
-            System.out.println(sum);
+            System.out.println(arrayNumber[begin+len-1] - arrayNumber[begin-1]);
         }
 
     }
