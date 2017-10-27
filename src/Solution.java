d38f92c37d20973b9730bd206b9e4b9abf098247
dell-master-linux <mayuanucas@qq.com>
2017-10-27 19:21:46 +0800

    子段求和

--- src/Solution.java
+++ src/Solution.java
@@ -1,3 +1,5 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
 import java.util.Scanner;
 
 /**
@@ -5,21 +7,22 @@ import java.util.Scanner;
  * @date 2017/10/26
  */
 public class Solution {
-    public static void main(String[] args){
-        Scanner sc = new Scanner(System.in);
+    public static void main(String[] args) throws Exception{
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
 
-        int n = sc.nextInt();
+        int n = Integer.parseInt(reader.readLine());
         // 数组中保存的是前n项的和
         long[] arrayNumber = new long[n+1];
 
         for (int i=1; i<=n; i++){
-            arrayNumber[i] = arrayNumber[i-1] + sc.nextLong();
+            arrayNumber[i] = arrayNumber[i-1] + Integer.parseInt(reader.readLine());
         }
 
-        int q = sc.nextInt();
+        int q = Integer.parseInt(reader.readLine());
         while (0 < q--){
-            int begin = sc.nextInt();
-            int len = sc.nextInt();
+            String[] str = reader.readLine().split(" ");
+            int begin = Integer.parseInt(str[0]);
+            int len = Integer.parseInt(str[1]);
             System.out.println(arrayNumber[begin+len-1] - arrayNumber[begin-1]);
         }
 
