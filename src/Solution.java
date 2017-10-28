5fd22b41e3a0c01b5c17e70115f775e0564ae3c7
dell-master-linux <mayuanucas@qq.com>
2017-10-28 09:30:39 +0800

    字段求和

--- src/Solution.java
+++ src/Solution.java
@@ -1,5 +1,3 @@
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
 import java.util.Scanner;
 
 /**
@@ -7,24 +5,8 @@ import java.util.Scanner;
  * @date 2017/10/26
  */
 public class Solution {
-    public static void main(String[] args) throws Exception{
-        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
-
-        int n = Integer.parseInt(reader.readLine());
-        // 数组中保存的是前n项的和
-        long[] arrayNumber = new long[n+1];
-
-        for (int i=1; i<=n; i++){
-            arrayNumber[i] = arrayNumber[i-1] + Integer.parseInt(reader.readLine());
-        }
-
-        int q = Integer.parseInt(reader.readLine());
-        while (0 < q--){
-            String[] str = reader.readLine().split(" ");
-            int begin = Integer.parseInt(str[0]);
-            int len = Integer.parseInt(str[1]);
-            System.out.println(arrayNumber[begin+len-1] - arrayNumber[begin-1]);
-        }
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
 
     }
 }
