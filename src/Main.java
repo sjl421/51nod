1b20e27485f08ba08267b36e2022f5ce51e3c9c8
mayuan <mayuanucas@qq.com>
2017-11-09 16:28:42 +0800

    删除不必要的包

--- src/Main.java
+++ src/Main.java
@@ -1,14 +1,6 @@
-import java.util.Scanner;
-
 public class Main {
 
     public static void main(String[] args){
-        Scanner sc = new Scanner(System.in);
-
-        while (sc.hasNext()){
-
-
-        }
 
         System.out.println("hello 51nod!");
     }
