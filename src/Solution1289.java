623f151b48ba85b75776a024fb97d46d0bf0f467
mayuan <mayuanucas@qq.com>
2017-12-05 15:30:58 +0800

    刷题

---
+++ src/Solution1289.java
@@ -0,0 +1,17 @@
+import java.io.*;
+import java.util.Arrays;
+
+/**
+ * @author mayuan
+ * @desc
+ * @date 2017/11/29
+ */
+public class Solution1289 {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in), 1 << 16);
+        BufferedWriter bufferedWriter = new BufferedWriter(new OutputStreamWriter(System.out), 1 << 16);
+
+
+    }
+}
