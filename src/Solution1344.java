cc8dcbf1e10fa33c47dcf629300210299144d716
mayuan <mayuanucas@qq.com>
2017-12-06 09:50:54 +0800

    刷题

---
+++ src/Solution1344.java
@@ -0,0 +1,18 @@
+import java.io.*;
+import java.util.Arrays;
+import java.util.Stack;
+
+/**
+ * @author mayuan
+ * @desc
+ * @date 2017/11/29
+ */
+public class Solution1344 {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in), 1 << 16);
+        BufferedWriter bufferedWriter = new BufferedWriter(new OutputStreamWriter(System.out), 1 << 16);
+
+
+    }
+}
