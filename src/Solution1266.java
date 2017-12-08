1566b2740850585c6dde0ac49ef6bbc49b2485dc
mayuan <mayuanucas@qq.com>
2017-12-08 19:09:59 +0800

    刷题

--- src/Solution1266.java
+++ src/Solution1266.java
@@ -5,11 +5,12 @@ import java.io.*;
  * @desc
  * @date 2017/11/29
  */
-public class Solution1014 {
+public class Solution1266 {
 
     public static void main(String[] args) throws IOException {
         BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in), 1 << 16);
         BufferedWriter bufferedWriter = new BufferedWriter(new OutputStreamWriter(System.out), 1 << 16);
+
 
     }
 }
