7672a70c87526f63ad3d754bdb2f1a4b613bcf3a
mayuan <mayuanucas@qq.com>
2017-12-08 22:27:09 +0800

    刷题

--- src/Solution1042.java
+++ src/Solution1042.java
@@ -5,7 +5,7 @@ import java.io.*;
  * @desc
  * @date 2017/11/29
  */
-public class Solution1266 {
+public class Solution1042 {
 
     public static void main(String[] args) throws IOException {
         BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in), 1 << 16);
