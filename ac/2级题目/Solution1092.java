3f238f85b4ee64ffd845951b30ba797c951eb7c9
mayuan <mayuanucas@qq.com>
2017-12-13 21:12:34 +0800

    添加解题思路

--- ac/2级题目/Solution1092.java
+++ ac/2级题目/Solution1092.java
@@ -11,6 +11,10 @@ public class Solution1092 {
         BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in), 1 << 16);
         BufferedWriter bufferedWriter = new BufferedWriter(new OutputStreamWriter(System.out), 1 << 16);
 
+        /**
+         * dp[i][j]数组代表：对应长度为 i 的 x 字符串, 长度为 j 的 y 字符串。
+         * 它们的最长公共子序列的长度为 dp[i][j]
+         */
         int[][] dp = new int[1005][1005];
 
         String x = bufferedReader.readLine();
@@ -18,11 +22,11 @@ public class Solution1092 {
 
         for (int i = 0; i < x.length(); i++) {
             for (int j = 0; j < y.length(); j++) {
-                int p = 0;
                 if (x.charAt(i) == y.charAt(j)) {
-                    p = 1;
+                    dp[i + 1][j + 1] = dp[i][j] + 1;
+                } else {
+                    dp[i + 1][j + 1] = Math.max(dp[i + 1][j], dp[i][j + 1]);
                 }
-                dp[i + 1][j + 1] = Math.max(dp[i][j] + p, Math.max(dp[i + 1][j], dp[i][j + 1]));
             }
         }
 
