c7d6c9933a92ad3e37750ae5f0defdc018034dd7
mayuan <mayuanucas@qq.com>
2017-12-28 21:00:41 +0800

    刷题

--- src/Solution1042.java
+++ src/Solution1042.java
@@ -11,5 +11,11 @@ public class Solution1042 {
         BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in), 1 << 16);
         BufferedWriter bufferedWriter = new BufferedWriter(new OutputStreamWriter(System.out), 1 << 16);
 
+        String[] temp = bufferedReader.readLine().split("\\s+");
+
+        long a = Long.parseLong(temp[0]), b = Long.parseLong(temp[1]);
+
+
+
     }
 }
