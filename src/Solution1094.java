af38c3409d4956d6169596815b15b5f8fdf2b450
mayuan <mayuanucas@qq.com>
2018-01-26 09:23:07 +0800

    解题

--- src/Solution1094.java
+++ src/Solution1094.java
@@ -5,15 +5,20 @@ import java.io.*;
  * @desc
  * @date 2017/11/29
  */
-public class Solution1042 {
+public class Solution1094 {
 
     public static void main(String[] args) throws IOException {
         BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in), 1 << 16);
         BufferedWriter bufferedWriter = new BufferedWriter(new OutputStreamWriter(System.out), 1 << 16);
 
         String[] temp = bufferedReader.readLine().split("\\s+");
+        int n = Integer.parseInt(temp[0]);
+        int k = Integer.parseInt(temp[1]);
+        int[] a = new int[n];
 
-        long a = Long.parseLong(temp[0]), b = Long.parseLong(temp[1]);
+        for (int i=0; i<n; i++){
+            a[i] = Integer.parseInt(bufferedReader.readLine());
+        }
 
 
 
