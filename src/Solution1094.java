8bf44eceddb88ac08bb67456af02df9d95c9d01f
mayuan <mayuanucas@qq.com>
2018-01-26 09:22:52 +0800

    配置文件

---
+++ src/Solution1094.java
@@ -0,0 +1,21 @@
+import java.io.*;
+
+/**
+ * @author mayuan
+ * @desc
+ * @date 2017/11/29
+ */
+public class Solution1042 {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in), 1 << 16);
+        BufferedWriter bufferedWriter = new BufferedWriter(new OutputStreamWriter(System.out), 1 << 16);
+
+        String[] temp = bufferedReader.readLine().split("\\s+");
+
+        long a = Long.parseLong(temp[0]), b = Long.parseLong(temp[1]);
+
+
+
+    }
+}
