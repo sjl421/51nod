100aed7aa41c229c8b2e08fc77a9b137371b9d37
mayuan <mayuanucas@qq.com>
2018-01-27 20:55:32 +0800

    解题

---
+++ src/Solution1521.java
@@ -0,0 +1,17 @@
+import java.io.*;
+
+/**
+ * @author mayuan
+ * @desc
+ * @date 2018/01/26
+ */
+public class Solution1521 {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in), 1 << 16);
+        BufferedWriter bufferedWriter = new BufferedWriter(new OutputStreamWriter(System.out), 1 << 16);
+
+
+
+    }
+}
